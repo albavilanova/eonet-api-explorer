1ca6d85c0d402cf0fa8eabc4a7f5ff451b577a45
Alba Vilanova Cortezon <alba.vilanova@outlook.com>
2024-04-06 18:37:42 +0200

    Retrieve data from EONET and save in class array

--- events.ts
+++ events.ts
@@ -1,5 +1,93 @@
-export const loadEvents = async () => {
-  const response = await fetch(`https://eonet.gsfc.nasa.gov/api/v3/events/`);
-  const results = await response.json();
-  return results
+interface EventSource {
+  id: string;
+  title: string;
+  url: string;
+}
+
+interface EventCategory {
+  id:
+    | "drought"
+    | "dustHaze"
+    | "earthquakes"
+    | "floods"
+    | "landslides"
+    | "manmade"
+    | "seaLakeIce"
+    | "severeStorms"
+    | "snow"
+    | "tempExtremes"
+    | "volcanoes"
+    | "waterColor"
+    | "wildfires";
+  title: string;
+}
+
+interface EventGeometry {
+  magnitudeValue: number;
+  magnitudeUnit: string;
+  date: string;
+  type: string;
+  coordinates: number[];
+}
+
+export class Event {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public closed: string,
+    public categories: EventCategory[],
+    public sources: EventSource[],
+    public geometry: EventGeometry[],
+    public geometryDates: string[]
+  ) {}
+}
+
+export const loadEvents = async (options: {
+  limit?: number;
+  start?: string;
+  end?: string;
+  source?: string;
+  category?: string;
+  status?: string;
+  bbox?: string;
+}) => {
+  // Append optional parameters to url
+  const url = new URL(`https://eonet.gsfc.nasa.gov/api/v3/events`);
+  Object.entries(options).forEach(([key, value]) => {
+    console.log(options);
+    url.searchParams.append(key, value.toString());
+  });
+
+  // Get results
+  const response = await fetch(url);
+  const results = (await response.json()) as any;
+
+  // Save them in an array
+  const events: Array<Event> = [];
+  for (const {
+    id,
+    title,
+    description,
+    closed,
+    categories,
+    sources,
+    geometry,
+    geometryDates,
+  } of results["events"]) {
+    events.push(
+      new Event(
+        id,
+        title,
+        description,
+        closed,
+        categories,
+        sources,
+        geometry,
+        geometryDates
+      )
+    );
+  }
+
+  return events;
 };
--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 import { loadEvents } from "./events.js";
 
-const events = await loadEvents();
+const limit = 1;
+const start = "2019-01-01";
+const end = "2022-01-01";
+
+const events = await loadEvents({ limit, start, end });
 console.log(events);
