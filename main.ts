a1fd037ba216c3db0dae935201046a7f28f9a7c9
Alba Vilanova Cortezon <alba.vilanova@outlook.com>
2024-04-07 19:18:32 +0200

    Make map full screen and add grid content to popups

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { writeFile } from "fs/promises";
 import { render } from "./render.js";
 import { loadEvents } from "./events.js";
 
-const limit = 16;
+const limit = 50;
 const start = "2023-01-01";
 const end = "2023-12-31";
 
--- render.ts
+++ render.ts
@@ -1,3 +1,4 @@
+import { marker } from "leaflet";
 import { Event } from "./events.js";
 import * as fs from "fs";
 
@@ -20,45 +21,28 @@ const head = (title: string) => `
 </head>`;
 
 const addMapElements = (events: Array<Event>) => {
-  let html = "";
+  let js = "";
   for (const event of events) {
     for (const geometry of event.geometry) {
       if (geometry.coordinates) {
-        html += `L.marker([${geometry.coordinates[1]}, ${geometry.coordinates[0]}]).addTo(mymap);`
+        var markerContent = `<div class="event">`
+        markerContent += `<div class="categories">`
+        for (const category of event.categories) {
+          markerContent += `<div class="category ${category.id}">${category.title}</div>`;
+        }
+        markerContent += `</div>`;
+        markerContent += `<div class="title">${event.title}</div>`;
+        if (geometry.magnitudeValue) {
+          markerContent += `<div class="magnitude">Magnitude: ${geometry.magnitudeValue} ${geometry.magnitudeUnit}</div>`;
+        }
+        markerContent += `</div>`;
+        js += `var marker = L.marker([${geometry.coordinates[1]}, ${geometry.coordinates[0]}]);`
+        js += `marker.addTo(mymap).bindPopup('${markerContent}');`
       }
-    }
-  }
-  return html;
-};
-
-const renderEvents = (events: Array<Event>) => {
-  let html = "";
-  for (const event of events) {
-    html += `<div class="event">
-        <div class="categories">`;
-    for (const category of event.categories) {
-      html += `<div class="category ${category.id}">${category.title}</div>
-                </div>`;
-    }
-    html += `
-        <div class="title">${event.title}</div>`;
-    html += `<div class="geometry">`;
-    for (const geometry of event.geometry) {
-      if (geometry.magnitudeValue) {
-        html += `<div class="magnitude">Magnitude: ${geometry.magnitudeValue} ${geometry.magnitudeUnit}</div>`;
-      }
-      if (geometry.coordinates) {
-        html += `<div class="coordinates">Lon: ${geometry.coordinates[0].toFixed(
-          2
-        )}, Lat: ${geometry.coordinates[1].toFixed(2)}</div>`;
-      }
-      // Show only 1 magnitude and set of coords, this can be expanded at per event level
       break
     }
-    html += `</div>`;
-    html += `</div>`;
   }
-  return html;
+  return js;
 };
 
 export const render = (events: Array<Event>) => {
@@ -71,14 +55,9 @@ export const render = (events: Array<Event>) => {
     </div>
     <div id="main-content">
         <div id="map"></div>
-        <div>
-            <div id="events">
-                ${renderEvents(events)}
-            </div>
-        </div>
     </div>
     <script>
-    var mymap = L.map('map').setView([0, 0], 1);
+    var mymap = L.map('map').setView([0, 0], 2);
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         maxZoom: 18,
         attribution: '&copy; Esri'
@@ -86,9 +65,11 @@ export const render = (events: Array<Event>) => {
     ${addMapElements(events)}
     </script>
     <footer>
+      <div>
         Earth Observatory Natural Event Tracker (EONET) Explorer
         <br>
         Developed by Alba Vilanova Cortezón with data from <a href="https://eonet.gsfc.nasa.gov/">NASA Goddard Space Flight Center</a>
+      </div>
     </footer>
   </body>
 </html>`;
