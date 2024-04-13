4a30482410311caa8e266dec95e754dfb186f289
Alba Vilanova Cortezon <alba.vilanova@outlook.com>
2024-04-13 18:55:45 +0200

    Add more events

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import { renderEvent } from "./renderEvent.js";
 import { loadEvents } from "./events.js";
 
 const limit = 500;
-const start = "2017-01-01";
+const start = "2000-01-01";
 const end = "2024-12-31";
 
 const events = await loadEvents({ limit, start, end });
